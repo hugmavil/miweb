2c36ff167ddf6259314d11788a313de9e814b7db
Hugo Aviles <hugmavil@gmail.com>
2023-05-23 19:43:23 -0500

    Agregar

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def init():
     return render_template('sitio/index.html')
 
+@app.route('/libros')
+def libros():
+    return render_template('/sitio/libros.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('/sitio/nosotros.html')
+
+@app.route('/admin')
+def admin_index():
+    return render_template('/admin/index.html')
+
+@app.route('/admin/login')
+def admin_login():
+    return render_template('/admin/login.html')
+
 if __name__=='__main__':
     app.run(debug=True)
