3d2f18e83269a9956d23dd6da65634126722704a
Hugo Aviles <hugmavil@gmail.com>
2023-06-02 14:03:28 -0500

    Cambios Hechos de practica

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -15,13 +15,27 @@ def libros():
 def nosotros():
     return render_template('/sitio/nosotros.html')
 
-@app.route('/admin')
+@app.route('/admin/')
 def admin_index():
     return render_template('/admin/index.html')
 
 @app.route('/admin/login')
 def admin_login():
     return render_template('/admin/login.html')
+
+@app.route('/admin/libros')
+def admin_libros():
+    return render_template('/admin/libros.html')
+
+@app.route('/admin/libros/guardar',methods=['POST'])
+def admin_libros_guardar():
+    _nombre=request.form['txtNombre']
+    _url=request.form['txtURL']
+    _archivo=request.files['txtImagen']
+    print(_nombre)
+    print(_url)
+    print(_archivo)
+    return redirect('/admin/libros')
 
 if __name__=='__main__':
     app.run(debug=True)
